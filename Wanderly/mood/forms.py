73ae1530c4b870961d79f2eca68bef6e04866d88
riley o'shea <roshea@uccs.edu>
2025-10-14 16:26:19 +0000

    updatedQuestionnaire

--- Wanderly/mood/forms.py
+++ Wanderly/mood/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 LIKERT = [(i, str(i)) for i in range(1, 6)]
 
+INTEREST_CHOICES = [
+    ('hiking', 'Hiking'),
+    ('water_adventures', 'Water adventures'),
+    ('sight_seeing', 'Sight seeing'),
+    ('museums', 'Museums'),
+    ('try_new_foods', 'Try new foods'),
+    ('concert_sporting', 'Attend a concert/sporting event'),
+    ('local_market', 'Visit a local market'),
+]
+
 class MoodForm(forms.Form):
     adventurous = forms.ChoiceField(
         label="How adventurous are you feeling?",
@@ -14,12 +24,9 @@ class MoodForm(forms.Form):
         widget=forms.RadioSelect
     )
 
-    what_do_you_enjoy = forms.CharField(
-        label="What kinds of things do you enjoy doing?",
+    what_do_you_enjoy = forms.MultipleChoiceField(
+        label="Select from the following some things you are interested in",
+        choices=INTEREST_CHOICES,
         required=True,
-        widget=forms.Textarea(attrs={
-            "rows": 3,
-            "class": "form-control",
-            "placeholder": "Enter some things you are interested in separated by commas and Wanderly will search for nearby activities"
-        })
+        widget=forms.CheckboxSelectMultiple
     )
--- Wanderly/mood/models.py
+++ Wanderly/mood/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class MoodResponse(models.Model):
     adventurous = models.IntegerField()
     energy = models.IntegerField()
-    what_do_you_enjoy = models.TextField()
+    what_do_you_enjoy = models.JSONField() 
     submitted_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
