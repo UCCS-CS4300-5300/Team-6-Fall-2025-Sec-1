8f62e037a06bda78634975da276eb96e95998848
Velvet-Vortex <123048270+Velvet-Vortex@users.noreply.github.com>
2025-10-20 08:04:14 -0600

    Feature/location based discovery (#31)
    * Adding Locations Based Discovery Feature
    
    * Better looking baseball card for place details
    
    * Adding tests for the API endpoint

--- Wanderly/Wanderly/urls.py
+++ Wanderly/Wanderly/urls.py
@@ -1,11 +1,29 @@
+"""
+URL configuration for Wanderly project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import index , sign_in, sign_out, auth_receiver, register, forgot_password
 
-
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+
+    # Location app urls
+    path('location-discovery/', include("location_based_discovery.urls")),
 
     # Mood app urls
     path('mood/', include("mood.urls", "mood")),
