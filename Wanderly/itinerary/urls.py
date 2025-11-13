dec8b8623e3eb05432a26c334c40480ffb5a2ac4
riley o'shea <roshea@uccs.edu>
2025-11-13 03:31:25 +0000

    updated itinerary - need to add stylesheet and add forms

--- Wanderly/itinerary/urls.py
+++ Wanderly/itinerary/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-
-from . import views
+from .views import itinerary
+from home.views import text_search
 
 app_name = "itinerary"
-
 urlpatterns = [
-    path("time-preferences/", views.itinerary_time_preferences, name="time_preferences"),
-    path("budget/", views.itinerary_budget, name="budget"),
-    path("location/", views.itinerary_location, name="location"),
-    path("summary/<int:itinerary_id>/", views.itinerary_summary, name="summary"),
+    path("", itinerary, name="itinerary"),
 ]
