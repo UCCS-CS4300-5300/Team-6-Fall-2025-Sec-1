c332658ea056580a6c5b103caee0a5bd8633ed27
gpw03 <gwilli17@uccs.edu>
2025-11-16 21:32:24 +0000

    Adding in pylinting for places_auto_complete, rated at a 10/10 now!

--- Wanderly/places_auto_complete/apps.py
+++ Wanderly/places_auto_complete/apps.py
@@ -1,6 +1,11 @@
+"""
+Application configuration for the places_auto_complete app.
+Defines the Django AppConfig used to register the app and configure its
+default settings.
+"""
 from django.apps import AppConfig
 
-
 class PlacesAutoCompleteConfig(AppConfig):
+    """App configuration class for the places_auto_complete Django application."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'places_auto_complete'
--- Wanderly/places_auto_complete/templatetags/places_auto.py
+++ Wanderly/places_auto_complete/templatetags/places_auto.py
@@ -1,8 +1,20 @@
+"""
+This creates a tag that can be used accross html docs
+It imports the script needed for the places auto 
+complete functionality. 
+You just need to add in the {% load static places_auto %}
+tag at the top of the html doc
+"""
 from django import template
 from django.conf import settings
 
 register = template.Library()
 
 @register.inclusion_tag("places_auto_complete/js_loader.html", takes_context=True)
-def places_js(context):
+def places_js():
+    """
+    Template tag that injects the Google Maps Browser API key into the
+    places_auto_complete JS loader template so autocomplete functionality
+    can be added to any HTML page.
+    """
     return {"GOOGLE_MAPS_BROWSER_KEY": getattr(settings, "GOOGLE_MAPS_BROWSER_KEY", "")}
